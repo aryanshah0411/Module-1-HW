b2ebf58fba1b7ed93a3f82fd01759069c622a40a
aryanshah0411 <aryanshah0411@gmail.com>
2026-07-17 21:44:11 +0530

    congratulations.py

---
+++ congratulations.py
@@ -0,0 +1,3 @@
+print("congratulations in getting a gold medal in state games!")
+print("congratulations on getting gold medal in state games!".upper())
+print("congratulations ".upper() + "congratulations on getting first place in state games!")
