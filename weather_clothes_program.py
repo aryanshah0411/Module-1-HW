6a6c801db3fa8f8edf40b56428165c36b588862d
aryanshah0411 <aryanshah0411@gmail.com>
2026-07-19 23:55:43 +0530

    weather_clothes_program

---
+++ weather_clothes_program.py
@@ -0,0 +1,5 @@
+weather = input("Is the weather cold today?")
+if weather == "Yes":
+    print("You should wear a jacket and pullover.")
+if weather =="No":
+    print("You should wear something light and soft.")
